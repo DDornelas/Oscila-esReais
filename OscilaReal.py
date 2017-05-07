9638aed768a8ff3635ed259f9cc7189f7033124c
Daniel Dornelas <dornelasdaniel@id.uff.br>
2017-05-07 11:47:09 -0300

    XxT e VxT gravidade Terra 2

--- OscilaReal.py
+++ OscilaReal.py
@@ -28,13 +28,11 @@ while t1<55:
 	t.append(t1)
 
 plt.figure(figsize=(6,5), dpi=96)
-#plt.axis([0,10,-1.5,1.5])
-#plt.xticks(np.linspace(0,10,6,endpoint=True))
+plt.axis([0,55,-1,1])
 
 ax=plt.gca()
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
-ax.autoscale()
 
 plt.rc('text', usetex=True)
 plt.rc('font', **{'sans-serif' : 'Arial', 'family' : 'sans-serif'})
@@ -45,7 +43,6 @@ plt.title(r'Oscila\c{c}\~{a}o Com Atrito', fontsize=12)
 plt.grid()
 plt.plot(t,x,'r-', linewidth=1, label="$x_{(t)}$")
 plt.plot(t,v,'b-', linewidth=1, label="$v_{(t)}$")
-#plt.plot(x,v)
 plt.legend(loc='upper right')
 plt.savefig("XxTVxT.pdf", dpi=96)
 plt.show()
