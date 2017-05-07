f7b826e994e86d85a343774ca439800f01271c48
Daniel Dornelas <dornelasdaniel@id.uff.br>
2017-05-07 12:31:16 -0300

    XxT e VxT gravidade Lua

--- OscilaReal.py
+++ OscilaReal.py
@@ -7,9 +7,9 @@ def verlet(xt,vt):
 	return p,f
 
 mi=0.003
-m=1
+m=1.56
 k=1
-g=9.8
+g=1.625
 n=m*g
 fat=mi*n
 dt=0.001
@@ -20,7 +20,7 @@ t1=0
 x=[1]
 v=[0]
 
-while t1<55:
+while t1<60:
 	t1+=dt
 	xt1,vt1=verlet(xt1,vt1)
 	x.append(xt1)
@@ -28,19 +28,21 @@ while t1<55:
 	t.append(t1)
 
 plt.figure(figsize=(6,5), dpi=96)
+plt.axis([0,55,-1,1])
 
 ax=plt.gca()
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
-ax.autoscale()
 
 plt.rc('text', usetex=True)
 plt.rc('font', **{'sans-serif' : 'Arial', 'family' : 'sans-serif'})
-plt.xlabel(r'Posi\c{c}\~{a}o(m)')
-plt.ylabel(r'Velocidade($\frac{m}{s}$)')
+plt.xlabel(r'Tempo(s)')
+plt.ylabel(r'Posi\c{c}\~{a}o(m) e Velocidade($\frac{m}{s}$)')
 
-plt.title(r'Oscila\c{c}\~{a}o Com Atrito Espa\c{c}o de Fases', fontsize=12)
+plt.title(r'Oscila\c{c}\~{a}o Com Atrito Na Lua', fontsize=12)
 plt.grid()
-plt.plot(x,v, 'r-', linewidth=1)
-plt.savefig("XxV.pdf", dpi=96)
+plt.plot(t,x, 'r-', linewidth=1, label="$x_{(t)}$")
+plt.plot(t,v, 'b-', linewidth=1, label="$v_{(t)}$")
+plt.legend(loc='upper right')
+plt.savefig("XxTVxTIS.pdf", dpi=96)
 plt.show()
