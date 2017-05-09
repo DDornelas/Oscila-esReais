1db7531684955891f44d78ec9f3a72bebcd4aae8
Daniel Dornelas <dornelasdaniel@id.uff.br>
2017-05-08 22:47:34 -0300

    Período massa 1 na terra

--- OscilaReal.py
+++ OscilaReal.py
@@ -7,7 +7,7 @@ def verlet(xt,vt):
 	return p,f
 
 mi=0.003
-m=1.7
+m=1
 k=1
 g=9.8
 n=m*g
@@ -19,6 +19,7 @@ vt1=0
 t1=0
 x=[1]
 v=[0]
+i=0
 
 while t1<25:
 	t1+=dt
@@ -26,24 +27,6 @@ while t1<25:
 	x.append(xt1)
 	v.append(vt1)
 	t.append(t1)
-
-plt.figure(figsize=(6,5), dpi=96)
-plt.axis([0,25,-1,1])
-
-ax=plt.gca()
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-
-
-plt.rc('text', usetex=True)
-plt.rc('font', **{'sans-serif' : 'Arial', 'family' : 'sans-serif'})
-plt.xlabel(r'Tempo(s)')
-plt.ylabel(r'Posi\c{c}\~{a}o(m) e Velocidade($\frac{m}{s}$)')
-
-plt.title(r'Oscila\c{c}\~{a}o Com Atrito Na Terra', fontsize=12)
-plt.grid()
-plt.plot(t,x, 'r-', linewidth=1, label="$x_{(t)}$")
-plt.plot(t,v, 'b-', linewidth=1, label="$v_{(t)}$")
-plt.legend(loc='upper right')
-plt.savefig("XxTVxTM.pdf", dpi=96)
-plt.show()
+	if xt1>-0.001 and xt1<0.001:
+		i+=1
+		print "%d: x=%f, t=%f\n"%(i, xt1, t1)
